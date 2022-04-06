4d4ff51278e22f6aa93d69bcb68200b6ae4946ce
Levy <levisilvas2018@gmail.com>
2022-04-06 16:28:03 -0300

    create class Posicao

--- Xadrez_Console/Xadrez_Console/Program.cs
+++ Xadrez_Console/Xadrez_Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Xadrez_Console.Tabuleiro;
 
 namespace Xadrez_Console
 {
@@ -6,7 +7,9 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var position = new Posicao(4,7);
+
+            Console.Write("Print: " + position);
         }
     }
 }
