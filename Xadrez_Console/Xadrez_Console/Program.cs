5436488f75d178867a7dca89909e0e1975b7a6b7
Levy <levisilvas2018@gmail.com>
2022-04-07 18:34:43 -0300

    improving method PrintTabuleiro

--- Xadrez_Console/Xadrez_Console/Program.cs
+++ Xadrez_Console/Xadrez_Console/Program.cs
@@ -8,10 +8,23 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
-            NotacaoXadrez pos = new NotacaoXadrez('c', 7);
-            Console.WriteLine(pos);
+            try
+            {
+                var tab = new Tabuleiro(8, 8);
 
-            Console.WriteLine(pos.ToPosicao());
+                tab.ColocarPeca(new Rei(Color.Preta, tab), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(Color.Preta, tab), new Posicao(1, 3));
+                tab.ColocarPeca(new Torre(Color.Preta, tab), new Posicao(0, 2));
+                tab.ColocarPeca(new Torre(Color.Branca, tab), new Posicao(4, 4));
+
+                Tela.PrintTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+            
         }
     }
 }
--- Xadrez_Console/Xadrez_Console/Tela.cs
+++ Xadrez_Console/Xadrez_Console/Tela.cs
@@ -9,18 +9,42 @@ namespace Xadrez_Console
         {
             for (int i = 0; i < tab.Linhas; i++)
             {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < tab.Colunas; j++)
                 {
                     if (tab.peca(i, j) == null)
                     {
-                        Console.Write("- ");
+                        Console.Write("-  ");
                     }
                     else
                     {
-                        Console.Write(tab.peca(i, j) + " ");
-                    }
+                        PrintPeca(tab.peca(i, j));
+                        Console.Write("  ");
+                    }  
                 }
                 Console.WriteLine();
+            }
+
+            //Console.WriteLine("  a b c d e f g h");
+
+            for (char c = 'a'; c <= 'h'; c++)
+            {
+                Console.Write("  " + c);
+            }
+        }
+
+        public static void PrintPeca(Peca peca)
+        {
+            if (peca.Cor == Color.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
