2e54a33c4e8a5bce5a2715b21d1832f1066af7ed
Levy <levisilvas2018@gmail.com>
2022-04-11 15:06:43 -0300

    implementing movement restrictions

--- Xadrez_Console/Xadrez_Console/Program.cs
+++ Xadrez_Console/Xadrez_Console/Program.cs
@@ -14,24 +14,38 @@ namespace Xadrez_Console
 
                 while (!partida.Terminada)
                 {
-                    Console.Clear();
-                    Tela.PrintTabuleiro(partida.Tab);
+                    try
+                    {
+                        Console.Clear();
+                        Tela.PrintTabuleiro(partida.Tab);
+                        Console.WriteLine();
+                        Console.WriteLine("Turno: " + partida.Turno);
+                        Console.WriteLine("Aguardando jogada: " + partida.JogadorAtual);
 
-                    Console.WriteLine();
-                    Console.Write("Origem: ");
-                    Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                        Console.WriteLine();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.LerPosicaoXadrez().ToPosicao();
+                        partida.validarPosicaoDeOrigem(origem);
 
-                    Console.WriteLine();
-                    bool[,] lancesPossiveis = partida.Tab.peca(origem).MovimentosPossiveis();
+                        Console.WriteLine();
+                        bool[,] lancesPossiveis = partida.Tab.peca(origem).MovimentosPossiveis();
 
-                    Console.Clear();
-                    Tela.PrintTabuleiro(partida.Tab, lancesPossiveis);
+                        Console.Clear();
+                        Tela.PrintTabuleiro(partida.Tab, lancesPossiveis);
 
-                    Console.WriteLine();
-                    Console.Write("Destino: ");
-                    Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+                        Console.WriteLine();
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.LerPosicaoXadrez().ToPosicao();
+                        partida.ValidarPosicaoDeDestino(origem, destino);
 
-                    partida.ExecutarMovimento(origem, destino);
+                        partida.RealizaJogada(origem, destino);
+                    }
+                    catch(TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                   
                 }
             }
             catch (TabuleiroException e)
--- Xadrez_Console/Xadrez_Console/Xadrez/PartidaXadrez.cs
+++ Xadrez_Console/Xadrez_Console/Xadrez/PartidaXadrez.cs
@@ -7,16 +7,15 @@ namespace Xadrez
     {
         public Tabuleiro Tab { get; private set; }
 
-        private int _turno;
-
-        private Color _jogadorAtual;
+        public int Turno { get; private set; }
+        public Color JogadorAtual { get; private set; }
         public bool Terminada { get; private set; }
 
         public PartidaXadrez()
         {
             Tab = new Tabuleiro(8, 8);
-            _turno = 1;
-            _jogadorAtual = Color.Branca;
+            Turno = 1;
+            JogadorAtual = Color.Branca;
             ColocarPecas();
         }
 
@@ -29,19 +28,61 @@ namespace Xadrez
             Tab.ColocarPeca(p,destino);
         }
 
+        public void RealizaJogada(Posicao origem, Posicao destino)
+        {
+            ExecutarMovimento(origem, destino);
+            Turno++;
+            MudarJogador();
+        }
+
+        public void validarPosicaoDeOrigem(Posicao pos)
+        {
+            if (Tab.peca(pos) == null)
+            {
+                throw new TabuleiroException("Não existe peça nessa posição de origem escolhida!");
+            }
+            if (JogadorAtual != Tab.peca(pos).Cor)
+            {
+                throw new TabuleiroException("A peça de origem escolhida não é sua!");
+            }
+            if (!Tab.peca(pos).ExisteMovimentosPossiveis())
+            {
+                throw new TabuleiroException("Não há lances permitidos para a peça de origem escolhida!");
+            }
+        }
+
+        public void ValidarPosicaoDeDestino(Posicao origem, Posicao destino)
+        {
+            if (!Tab.peca(origem).PodeMoverPara(destino))
+            {
+                throw new TabuleiroException("Posição de destino inválida!");
+            }
+        }
+
+        private void MudarJogador()
+        {
+            if(JogadorAtual == Color.Branca)
+            {
+                JogadorAtual = Color.Preta;
+            }
+            else
+            {
+                JogadorAtual = Color.Branca;
+            }
+        }
         private void ColocarPecas()
         {
             Tab.ColocarPeca(new Rei(Color.Branca, Tab), new NotacaoXadrez('e',1).ToPosicao());
             Tab.ColocarPeca(new Torre(Color.Branca, Tab), new NotacaoXadrez('a', 1).ToPosicao());
             Tab.ColocarPeca(new Torre(Color.Branca, Tab), new NotacaoXadrez('h', 1).ToPosicao());
-
-            Tab.ColocarPeca(new Torre(Color.Branca, Tab), new NotacaoXadrez('h', 8).ToPosicao());
-            Tab.ColocarPeca(new Torre(Color.Branca, Tab), new NotacaoXadrez('a', 8).ToPosicao());
             Tab.ColocarPeca(new Torre(Color.Branca, Tab), new NotacaoXadrez('d', 1).ToPosicao());
             Tab.ColocarPeca(new Torre(Color.Branca, Tab), new NotacaoXadrez('d', 2).ToPosicao());
             Tab.ColocarPeca(new Torre(Color.Branca, Tab), new NotacaoXadrez('c', 1).ToPosicao());
             Tab.ColocarPeca(new Torre(Color.Branca, Tab), new NotacaoXadrez('c', 2).ToPosicao());
-            Tab.ColocarPeca(new Rei(Color.Branca, Tab), new NotacaoXadrez('e', 8).ToPosicao());
+
+            Tab.ColocarPeca(new Torre(Color.Preta, Tab), new NotacaoXadrez('h', 8).ToPosicao());
+            Tab.ColocarPeca(new Torre(Color.Preta, Tab), new NotacaoXadrez('a', 8).ToPosicao());
+            Tab.ColocarPeca(new Rei(Color.Preta, Tab), new NotacaoXadrez('e', 8).ToPosicao());
         }
     }
 }
