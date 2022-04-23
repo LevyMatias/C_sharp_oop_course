84265f91bb417f10aae7a76ac95f519063ad6f3e
Levy <levisilvas2018@gmail.com>
2022-04-23 13:48:29 -0300

    add problem with interface

---
+++ Interfaces/LocadoraCarros/Program.cs
@@ -0,0 +1,37 @@
+﻿using LocadoraCarros.Entities;
+using LocadoraCarros.Services;
+using System;
+using System.Globalization;
+
+namespace LocadoraCarros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data:");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.Write("Enter price per hour: ");
+            double priceHour = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            
+            Console.Write("Enter price per Day: ");
+            double priceDay = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            var carRental = new CarRental(start, finish, new Vehicle(model));
+
+            var rentalService = new RentalService(priceDay,priceHour, new BrazilTaxServices());
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("Invoice: ");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
---
+++ Interfaces/LocadoraCarros/Services/RentalService.cs
@@ -0,0 +1,38 @@
+﻿using LocadoraCarros.Entities;
+using System;
+
+namespace LocadoraCarros.Services
+{
+    class RentalService
+    {
+        public double PricePerDay { get; private set; }
+        public double PricePerHour { get; private set; }
+
+        private ITaxService _taxServices;
+
+        public RentalService(double pricePerDay, double pricePerHour, ITaxService taxService)
+        {
+            PricePerDay = pricePerDay;
+            PricePerHour = pricePerHour;
+            _taxServices = taxService;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0;
+
+            if(duration.TotalHours <= 12)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+
+            double tax = _taxServices.Tax(basicPayment);
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
