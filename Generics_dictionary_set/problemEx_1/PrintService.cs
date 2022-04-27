ae2c5f4f566c0c2b5f27ddefd7b2002bd1970fb6
Levy <levisilvas2018@gmail.com>
2022-04-27 16:48:40 -0300

    solving problem with generics

---
+++ Generics_dictionary_set/problemEx_1/PrintService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace problemEx_1
+{
+    class PrintService<T>
+    {
+        private T[] values = new T[10];
+        private int count = 0;
+        public void AddValue(T value)
+        {
+            if(count == 10)
+            {
+                throw new InvalidOperationException("Print service is full");
+            }
+            values[count] = value;
+            count++;
+        }
+
+        public T First()
+        {
+            if (count == 0)
+            {
+                throw new InvalidOperationException("Print service is Empty");
+            }
+            return values[0];
+        }
+
+        public void Print()
+        {
+            Console.Write("[");
+            for(var i = 0; i < count - 1; i++)
+            {
+                Console.Write(values[i] + ", ");
+            }
+            if(count > 0)
+            {
+                Console.Write(values[count - 1]);
+            }
+
+            Console.Write("]");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Generics_dictionary_set/problemEx_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace problemEx_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var print = new PrintService<int>();
+            Console.Write("How many values: ");
+            int n = int.Parse(Console.ReadLine());
+            for (var i = 0; i<n; i++)
+            {
+                int values = int.Parse(Console.ReadLine());
+                print.AddValue(values); 
+            }
+
+            print.Print();
+            Console.WriteLine("First: " + print.First());
+
+        }
+    }
+}
