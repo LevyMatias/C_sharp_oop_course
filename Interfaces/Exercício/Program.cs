e65a1522a3462c31e87460eb6fe178d05f94912b
Levy <levisilvas2018@gmail.com>
2022-04-27 11:30:44 -0300

    exercise with Icomparable interface

---
+++ Interfaces/Exercício/Program.cs
@@ -0,0 +1,36 @@
+﻿using Exercício.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exercício
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\EstudosLevy\Outros\Caracteres\Ex.txt";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (Employee emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
