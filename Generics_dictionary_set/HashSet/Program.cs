8ff7dc549a13f6be443b90e6b8f879bd2da37006
Levy <levisilvas2018@gmail.com>
2022-05-02 11:16:50 -0300

    demo hashset generic class

---
+++ Generics_dictionary_set/HashSet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+            set.Add("Strewberry");
+            set.Add("Orange");
+            set.Add("Bananas");
+            set.Add("Apple");
+
+            Console.WriteLine(set.Contains("Bananas"));
+
+            foreach (String f in set)
+            {
+                Console.WriteLine(f);
+            }
+        }
+    }
+}
