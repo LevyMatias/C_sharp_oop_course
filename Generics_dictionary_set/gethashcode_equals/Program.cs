d86b0f0a6e8f6a7ed6bdf2d50cb942bc3134e737
Levy <levisilvas2018@gmail.com>
2022-05-02 11:10:43 -0300

    equals and hash code

---
+++ Generics_dictionary_set/gethashcode_equals/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using gethashcode_equals.Entities;
+
+namespace gethashcode_equals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client c1 = new Client() { Name = "Jose", Email = "jose.email@eu.com" };
+            Client c2 = new Client() { Name = "Julia", Email = "julia.email@eu.com" };
+
+            Console.WriteLine(c1.Equals(c2));
+            Console.WriteLine(c2 == c1);
+            Console.WriteLine(c1.GetHashCode());
+            Console.WriteLine(c2.GetHashCode());
+        }
+    }
+}
