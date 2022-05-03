1f1354040c24b77a60e157e7edb75ac346396c80
Levy <levisilvas2018@gmail.com>
2022-05-03 16:13:30 -0300

    problem exemple with Set

---
+++ Generics_dictionary_set/Set_1/Entities/LogsRecord.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Set_1.Entities
+{
+    class LogsRecord
+    {
+        public string UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is LogsRecord))
+            {
+                return false;
+            }
+            LogsRecord other = obj as LogsRecord;
+            return UserName.Equals(other.UserName);
+        }
+    }
+}
---
+++ Generics_dictionary_set/Set_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Set_1.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Set_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<LogsRecord> set = new HashSet<LogsRecord>();
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(" ");
+                        string name = line[0];
+                        DateTime instant = DateTime.Parse(line[1]);
+                        set.Add(new LogsRecord { UserName = name, Instant = instant });
+                    }
+                    Console.WriteLine("Total users: " + set.Count);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
