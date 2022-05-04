ed23d32288d6c4642ccb87a0b1b5debcd67caf4b
Levy <levisilvas2018@gmail.com>
2022-05-04 15:41:08 -0300

    dictionary exercise

---
+++ Generics_dictionary_set/Dictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> dic = new Dictionary<string,int>();
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        int voto = int.Parse(line[1]);
+
+                        if (dic.ContainsKey(name))
+                        {
+                            dic[name] += voto;
+                        }
+                        else
+                        {
+                            dic[name] = voto;
+                        }
+                    }
+                }
+
+                foreach (var item in dic)
+                {
+                    Console.WriteLine(item.Key + ": " + item.Value);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
